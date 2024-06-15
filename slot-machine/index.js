f4a7b69d0cab8d0624f7a271de120a89df3e408b
avd <aravind19112003@gmail.com>
2024-06-15 15:04:37 +0530

    Steps involved

---
+++ slot-machine/index.js
@@ -0,0 +1,12 @@
+// 1. Deposit some money
+// 2. Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if the user won
+// 6 . Give or Take money
+// 7 . Play again
+
+
+// function deposit() {
+//     return 1
+// }
