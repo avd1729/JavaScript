c9c69db1be2badff01939b611fd02ba02e5cf924
avd <aravind19112003@gmail.com>
2024-06-15 16:13:01 +0530

    Spin the machine

--- slot-machine/index.js
+++ slot-machine/index.js
@@ -13,17 +13,17 @@ const ROWS = 3;
 const COLS = 3;
 
 const SYMBOLS_COUNT = {
-    "A":2,
-    "B":4,
-    "C":6,
-    "D":8
+    A:2,
+    B:4,
+    C:6,
+    D:8
 }
 
 const SYMBOL_VALUES = {
-    "A":5,
-    "B":4,
-    "C":3,
-    "D":2
+    A:5,
+    B:4,
+    C:3,
+    D:2
 }
 
 
@@ -68,9 +68,36 @@ const getBet = (balance , noOfLines) => {
         }
     }
 
-}
+};
+
+const spin = () => {
+    const symbols = [];
+    for (const [symbol , count] of Object.entries(SYMBOLS_COUNT)){
+        for(let i=0 ; i<count ; i++){
+            symbols.push(symbol);
+        }
+    }
+    // console.log(symbols);
+
+    const reels = [];
+    for (let i = 0; i < COLS; i++) {
+
+      reels.push([]);
+      const reelSymbols = [...symbols];
+
+      for (let j = 0; j < ROWS; j++) {
+
+        const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+        const selectedSymbol = reelSymbols[randomIndex];
+        reels[i].push(selectedSymbol);
+        reelSymbols.splice(randomIndex, 1); // index , no of elements to remove
+        
+        }
+    }
+
+    return reels;
+};
 
 let balance = deposit();
 const noOfLines = lines();
-getBet(balance , noOfLines);
-
+const bet = getBet(balance , noOfLines);
