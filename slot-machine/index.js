1df1818a056c41f852cd2ad6f86bdf8152725080
avd <aravind19112003@gmail.com>
2024-06-15 15:47:21 +0530

    Add constants

--- slot-machine/index.js
+++ slot-machine/index.js
@@ -8,6 +8,25 @@
 
 const prompt = require("prompt-sync")();
 
+
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    "A":2,
+    "B":4,
+    "C":6,
+    "D":8
+}
+
+const SYMBOL_VALUES = {
+    "A":5,
+    "B":4,
+    "C":3,
+    "D":2
+}
+
+
 const deposit = () => {
 
     while(true){
@@ -53,4 +72,5 @@ const getBet = (balance , noOfLines) => {
 
 let balance = deposit();
 const noOfLines = lines();
+getBet(balance , noOfLines);
 
